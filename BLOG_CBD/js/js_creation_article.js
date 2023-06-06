730bbee2cebfc7e181be16115975bf779af3dc23
TonTonHigo <hugoh420@outlook.fr>
2023-06-06 15:24:49 +0400

    mise à jour des pages article et crétion d'article avec js

---
+++ BLOG_CBD/js/js_creation_article.js
@@ -0,0 +1,26 @@
+function boucle(){
+    for(let i = 1; i <= 10; i++){
+        carte();
+    }
+}
+
+function carte(){
+    let input_un = document.getElementById('input1').value;
+    console.log(input_un);
+    let input_deux = document.getElementById('input2').value;
+    let input_trois = document.getElementById('input3').value;
+
+    const para = document.createElement("img");
+    para.setAttribute('id','image');
+    const para2 = document.createElement("h2");
+    const para3 = document.createElement("p");
+
+
+    para.src = "../images/" + input_un;
+    para2.innerHTML = input_deux;
+    para3.innerHTML = input_trois;
+
+    document.getElementById("fond").appendChild(para);
+    document.getElementById("fond").appendChild(para2);
+    document.getElementById("fond").appendChild(para3);
+}
